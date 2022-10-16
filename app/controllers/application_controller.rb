aa1d2643316256559abfb90c0907922e29b29b48
esun33 <eleanor.sun@chicagobooth.edu>
2022-10-16 02:53:12 +0000

    Progress on rock and scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,25 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
   def play_rock
-
     render({:template => "game_templates/user_rock.html.erb"})
+  end
+
+  def home_page
+    render({:template => "game_templates/rules.html.erb"})
+  end
+
+  def play_paper
+    @comp_move = ["rock", "scissors", "paper"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won!"
+    elsif @comp_move == "paper"
+      @outcome = "tied!"
+    elsif @comp_move == "scissors"
+      @outcome = "lost!"
+    end
+
+    render({:template => "game_templates/user_paper.html.erb"})
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get("/", {:controller => "application", :action => "home_page"})
+
   get("/rock", {:controller => "application", :action => "play_rock"})
+
+  get("/paper", {:controller => "application", :action => "play_paper"})
+
+
 
 end
